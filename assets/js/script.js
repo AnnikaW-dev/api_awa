c54993d37198b96cf1a4123912b33f05915fa48a
Annika Warm <warmannika@gmail.com>
2025-01-17 17:36:06 +0000

    Making a Request - Part 1 & 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-LOqD58lMs_jBEcn15r2gO8LHm1M
+const API_KEY = "LOqD58lMs_jBEcn15r2gO8LHm1M";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    }else {
+        throw new (data.error);
+    }
+
+    function displayStatus() {
+        let heading = "API Key Status";
+        let results = `<div>Your key is valid until</div>`;
+        results += `<div class="key-status">${data.expiry}</div>`;
+
+        document.getElementById("resultsModalTitle").innerText = heading;
+        document.getElementById("results-content").innerHTML = results;
+        resultsModal.show();
+    }
+
+}
